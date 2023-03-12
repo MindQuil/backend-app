d9efd0e928b79251e2f1154683b72e5b6c06196e
Ahmed Olawale A <ahmed4donola1@gmail.com>
2023-03-12 04:04:12 +0000

    [Update]: refractor server file

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-});
-
 // middlewares
 app.use('/users', userRoutes);
 app.use('/therapists', therapistRoutes);
